bbc164631f52ce077af66579f9bec703ed3dbdf4
James <sep7696@gmail.com>
2023-12-31 21:37:06 +0330

    Catalog Context is Intilized.

---
+++ Source/Catalog.Api/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+         
+    }
+}
