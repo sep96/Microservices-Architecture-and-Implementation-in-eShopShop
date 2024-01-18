980ec8f06bc437f2611def63a10011d77f19f364
James <sep7696@gmail.com>
2024-01-18 23:20:21 +0330

    Feat : Add ShoppingCart and ShoppingCartItem Entity

---
+++ Source/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new();
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;  
+                foreach (ShoppingCartItem item in Items)
+                {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+            set
+            {
+
+            };
+        }
+    }
+}
---
+++ Source/Basket/Basket.Api/Entities/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
