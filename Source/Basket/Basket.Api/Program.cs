aeb1d6d8f996a58c9a0b9349aa04c4ca2e474553
James <sep7696@gmail.com>
2024-01-21 21:32:22 +0330

    Fix : change depends_on docker compse file.
    threre is a mistake that for basket api depends_on set catalogdb insted of basketdb and redis cant run .

--- Source/Basket/Basket.Api/Program.cs
+++ Source/Basket/Basket.Api/Program.cs
@@ -1,4 +1,5 @@
 using Basket.Api.Repositories;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddStackExchangeRedisCache(opt => {
     opt.Configuration = builder.Configuration["CacheSetting:ConnectionString"];
 });
+//builder.Services.AddHealthChecks().AddRedis(Configuration["CacheSettings:ConnectionString"], "Redis Health", HealthStatus.Degraded);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
